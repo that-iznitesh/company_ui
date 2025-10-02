de1d4be023f812804159187341f9355253f322c5
Nitesh Kumar <nitesh565golu@gmail.com>
2025-10-02 14:50:04 +0530

    4th commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,10 +17,10 @@ function Home() {
         />    
          <div className="absolute inset-0 flex flex-col justify-center items-center text-center bg-black/40">
           <h1 className="text-4xl md:text-6xl font-bold text-white mb-4">
-           Welcome to My Website
+         Prepare for IELTS Sucess
           </h1>
           <p className="text-lg md:text-2xl text-gray-200 mb-6">
-           Build modern websites with React & Tailwind CSS
+           This is the ai based platform to learn IELTS
           </p>
           <button className="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white rounded-lg text-lg font-semibold shadow-lg transition">
            Get Started
